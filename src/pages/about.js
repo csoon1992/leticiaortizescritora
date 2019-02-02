b8221e7293bbce22a9a4f0a1ab1aeea98f63631a
Antonio Gázquez <piticonejo@gmail.com>
2019-02-02 18:55:00 +0100

    Add contact form

--- src/pages/about.js
+++ src/pages/about.js
@@ -7,6 +7,7 @@ import dogIllustration from '../images/dog-illustration.svg';
 import AmazonButton from '../components/AmazonButton';
 import EciButton from '../components/EciButton';
 import LetrameButton from '../components/LetrameButton';
+import Contact from '../sections/Contact';
 
 const AboutPage = () => (
     <Layout>
@@ -33,26 +34,7 @@ const AboutPage = () => (
             </div>
         </div>
 
-        <div className="flex flex-col md:flex-row items-center mb-3">
-            <div className="md:w-2/3 md:mr-8">
-                <p className="border-l-4 border-grey-darkest font-serif leading-loose pl-4 text-justify">
-                    The point is... to live one's life in the full complexity of
-                    what one is, which is something much darker, more
-                    contradictory, more of a maelstrom of impulses and passions,
-                    of cruelty, ecstacy, and madness, than is apparent to the
-                    civilized being who glides on the surface and fits smoothly
-                    into the world.
-                </p>
-
-                <p className="font-bold mt-4 text-right text-xs uppercase">
-                    – Thomas Nagel
-                </p>
-            </div>
-
-            <div className="w-2/3 md:w-1/3">
-                <img src={dogIllustration} alt="A dog relaxing" />
-            </div>
-        </div>
+        <Contact />
     </Layout>
 );
 
