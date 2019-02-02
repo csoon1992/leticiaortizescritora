b2ec84dc4bef26587b677468c8b97c4fdcae9bfc
Antonio Gázquez <piticonejo@gmail.com>
2019-02-02 13:25:19 +0100

    Cleanup default content

--- gatsby-config.js
+++ gatsby-config.js
@@ -8,6 +8,12 @@ module.exports = {
     plugins: [
         'gatsby-plugin-react-helmet',
         {
+            resolve: 'gatsby-plugin-google-fonts',
+            options: {
+                fonts: ['lora', 'source sans pro'],
+            },
+        },
+        {
             resolve: 'gatsby-source-filesystem',
             options: {
                 name: 'images',
--- src/components/layout.js
+++ src/components/layout.js
@@ -18,34 +18,9 @@ const Layout = ({ children }) => (
         `}
         render={data => (
             <div className="flex flex-col font-sans min-h-screen text-grey-darkest">
-                <Header siteTitle={data.site.siteMetadata.title} />
-
                 <div className="flex flex-col flex-1 md:justify-center max-w-xl mx-auto px-4 py-8 md:p-8 w-full">
                     {children}
                 </div>
-
-                <footer className="bg-blue">
-                    <div className="flex justify-between max-w-xl mx-auto p-4 md:p-8 text-sm">
-                        <p className="text-white">
-                            Created by{' '}
-                            <a
-                                href="https://taylorbryant.blog"
-                                className="font-bold no-underline text-white"
-                            >
-                                Taylor Bryant
-                            </a>
-                        </p>
-
-                        <p>
-                            <a
-                                href="https://github.com/taylorbryant/gatsby-starter-tailwind"
-                                className="font-bold no-underline text-white"
-                            >
-                                GitHub
-                            </a>
-                        </p>
-                    </div>
-                </footer>
             </div>
         )}
     />
