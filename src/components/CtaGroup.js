57f33c7b67dfbb693f1479a43a1ee124391a8d33
Cristina Soler <csoon1992@gmail.com>
2019-02-03 20:38:06 +0100

    Fix mobile styles

--- src/components/CtaGroup.js
+++ src/components/CtaGroup.js
@@ -5,15 +5,15 @@ import EciButton from './EciButton';
 import LetrameButton from './LetrameButton';
 
 const CtaGroup = ({ className, href }) => (
-    <div className="cta mt-6 flex items-strech">
+    <div className="cta mt-6 flex items-strech flex-wrap sm:flex-no-wrap">
         <AmazonButton
-            className="mr-3"
+            className="mr-3 mb-3 sm:mb-0"
             href="https://www.amazon.es/dp/8417779310/ref=cm_sw_em_r_mt_dp_U_iRBvCb3BTG6MR"
         />
 
         <EciButton
             href="https://www.elcorteingles.es/ebooks/tagus-9788417818395-kika-nada-pasa-hasta-que-pasa-ebook/"
-            className="mr-3"
+            className="mr-3 mb-3 sm:mb-0"
         />
 
         <LetrameButton 
