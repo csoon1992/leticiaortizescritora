2bf6cb47515a9ec484ddee32d6d2829e5643a588
Cristina Soler <csoon1992@gmail.com>
2019-02-03 22:16:32 +0100

    Fix styles

--- src/components/HeaderCaption.js
+++ src/components/HeaderCaption.js
@@ -3,11 +3,11 @@ import CtaGroup from '../components/CtaGroup';
 
 const HeaderCaption = () => (
     <div className="caption-wrapper pl-8 md:pl-10 md:px-8 lg:pl-16 lg:pr-0 flex flex-row md:items-start md:pt-10 lg:pt-0 lg:items-center md:mx-auto lg:mx-0">
-        <div className="caption max-w-1/2 sm:max-w-sm">
-            <h2 className="text-xl sm:text-4xl lg:text-4xl xl:text-6xl font-serif font-normal text-black lg:w-4/5">
+        <div className="caption max-w-1/2 sm:max-w-sm md:max-w-1/2">
+            <h2 className="text-xl sm:text-4xl lg:text-4xl xl:text-6xl font-serif font-normal text-black lg:w-4/5 xl:w-full">
                 ¡Primer libro, ya a la venta!
             </h2>
-            <div className="text-lg font-normal max-w-sm mt-4 leading-normal hidden sm:block lg:w-3/5">
+            <div className="text-lg font-normal max-w-sm mt-4 leading-normal hidden sm:block lg:w-3/5 xl:w-full">
                 <p>
                     Kika hará que te enamores de su humor y te sacará sonrisas
                     en cada capítulo.
