0fcd286f9b4841a7dd91c1f42d4e294d393ffa37
Antonio Gázquez <piticonejo@gmail.com>
2019-02-03 18:36:17 +0100

    Add Google Analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -59,6 +59,19 @@ module.exports = {
                 purgeOnly: ['src/css/style.css'], // Purge only tailwind
             },
         },
+
+        {
+            resolve: 'gatsby-plugin-google-analytics',
+            options: {
+                trackingId: 'UA-129053715-2',
+                // Puts tracking script in the head instead of the body
+                head: false,
+                // Setting this parameter is optional
+                anonymize: true,
+                // Setting this parameter is also optional
+                respectDNT: true,
+            },
+        },
         // this (optional) plugin enables Progressive Web App + Offline functionality
         // To learn more, visit: https://gatsby.app/offline
         // 'gatsby-plugin-offline',
