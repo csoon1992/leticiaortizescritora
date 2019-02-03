8acd6a76617fad49e6fa1b780c56495de0034cfe
Antonio Gázquez <piticonejo@gmail.com>
2019-02-03 20:02:45 +0100

    Fix mobile styles

--- src/components/EciButton.js
+++ src/components/EciButton.js
@@ -26,12 +26,13 @@ const EciButton = ({ className, href }) => (
                 <a
                     href={href}
                     target="_blank"
-                    className={`block text-left px-3 py-1 bg-eci text-white font-sans no-underline hover:bg-eci-darker w-32 rounded-lg ${className ||
+                    rel="noopener noreferrer"
+                    className={`block text-left px-3 py-1 bg-eci text-white font-sans no-underline hover:bg-eci-darker w-24 md:w-32 rounded-lg ${className ||
                         ''}`}
                 >
-                    <span className="text-sm">Disponible en</span>
+                    <span className="text-xs md:text-sm">Disponible en</span>
                     <Img
-                        className="block mt-1 ml-0 mb-0"
+                        className="block mt-2 ml-0 mb-0 w-full"
                         alt="Eci"
                         key={data.logos.edges[0].node.childImageSharp.fluid.src}
                         fluid={data.logos.edges[0].node.childImageSharp.fluid}
